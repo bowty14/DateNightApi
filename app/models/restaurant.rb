766a2b3c2055455835f4346dd1992ae65f55d23d
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-17 15:25:27 -0700

    update restaurant model to include pg search scope and validations.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,21 @@
 class Restaurant < ApplicationRecord
+  include PgSearch::Model 
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :number, presence: true
+  validates :menu, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
+  validates :reservation, presence: true
+  validates :website, presence: true
+
+
+  pg_search_scope :search_restaurants, against: [:name],
+  using: {
+    tsearch: {
+      any_word: true,
+      prefix: true
+    }
+  }
 end
