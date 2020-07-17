c5b78bfc8c501e927a0f9ad5d2cd5e2c282eb480
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-17 10:06:02 -0700

    add restaurant model, add addional endpoints, add first restaurant to database.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,9 +5,33 @@ class RestaurantsController < ApplicationController
     json_response(@restaurants)
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    json_response(@restaurant)
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    json_response(@restaurant)
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def restaurant_params
+    params.permit(:name, :address, :number, :menu, :reservation, :website)
   end
 end
