9827061e2acf2b2fe6495257eb25f80e286e9832
Tyler Bowerman <44910152+bowty14@users.noreply.github.com>
2020-07-22 13:48:00 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 restaurant_list = [
-  ['Clark and Lewis','1001 SE Water Ave, Portland, OR 97214',('503) 235-2294','https://www.clarklewispdx.com/menus/dinner/','https://www.opentable.com/r/clarklewis-portland?avt=eyJ2IjoxLCJtIjowLCJwIjowfQ&corrid=36d4951b-7a16-4712-ac93-748e0c350fc9&p=2&sd=2020-07-17+19%3A00','https://www.clarklewispdx.com/','Northwest food','$31 to $50']
+  ['Clark and Lewis','1001 SE Water Ave, Portland, OR 97214','(503) 235-2294','https://www.clarklewispdx.com/menus/dinner/','https://www.opentable.com/r/clarklewis-portland?avt=eyJ2IjoxLCJtIjowLCJwIjowfQ&corrid=36d4951b-7a16-4712-ac93-748e0c350fc9&p=2&sd=2020-07-17+19%3A00','https://www.clarklewispdx.com/','Northwest food','$31 to $50']
 ]
 
 
