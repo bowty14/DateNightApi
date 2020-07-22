3f547fd7e63a10362db5156a9671aca5d0164913
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-22 13:19:56 -0700

    add a seedfile for restaurant data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,8 @@
-class Seed
+restaurant_list = [
+  ['Clark and Lewis','1001 SE Water Ave, Portland, OR 97214',('503) 235-2294','https://www.clarklewispdx.com/menus/dinner/','https://www.opentable.com/r/clarklewis-portland?avt=eyJ2IjoxLCJtIjowLCJwIjowfQ&corrid=36d4951b-7a16-4712-ac93-748e0c350fc9&p=2&sd=2020-07-17+19%3A00','https://www.clarklewispdx.com/','Northwest food','$31 to $50']
+]
 
-  def self.begin 
-    seed = Seed.new
-    seed.generate_restaurants
-  end
 
-  def generate_restaurants
-    20.times do |i|
-      restaurant = Restaurant.create!(
-        name: Faker::Restaurant.name,
-        address: Faker::Address.street_address,
-        number: Faker::Number.number(digits: 10),
-        menu: Faker::Internet.domain_name(domain: "example"),
-        category: Faker::Restaurant.type,
-        price: Faker::Number.decimal(l_digits: 2),
-        reservation: Faker::Internet.domain_name(domain: "example"),
-        website: Faker::Internet.domain_name(domain: "example")
-      )
-      puts 'Restaurant #{i}: This restaurants info is #{restaurant.name}, #{restaurant.address},#{restaurant.number},#{restaurant.menu},#{restaurant.category},#{restaurant.price},#{restaurant.reservation},#{restaurant.website}'
-    end
-  end
-
+restaurant_list.each do |name,address,number,menu,reservation,website, category,price|
+  Restaurant.create( restaurant_name: name, restaurant_address: address, restaurant_number: number, restaurant_menu: menu, restaurant_reservation: reservation, restaurant_website: website, restaurant_category: category, restaurant_price: price)
 end
-
-  Seed.begin
-      
